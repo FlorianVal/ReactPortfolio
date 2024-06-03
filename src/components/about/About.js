33749cf9589e79442e0a11f8d3f7b19f64d8f0bd
Florian Valade <florian_val@outlook.fr>
2024-06-04 00:04:50 +0200

    update for eng and fr cv

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,24 +1,43 @@
 import React, { useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Tabs, Tab } from "@mui/material";
 import { Document, pdfjs, Page } from "react-pdf";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import Typography from '@mui/material/Typography';
+
 
 pdfjs.GlobalWorkerOptions.workerSrc = "./pdf.worker.js";
 
 export default function About() {
-  const [numPagesEn, setNumPagesEn] = useState(1);
-  const [numPagesFr, setNumPagesFr] = useState(1);
+  const [value, setValue] = React.useState(0);
   const [pageNumber] = useState(1);
+  
 
-  function onDocumentLoadSuccess({ numPages }) {
-    if (documentName === "main_en.pdf") {
-      setNumPagesEn(numPages);
-    } else if (documentName === "main_fr.pdf") {
-      setNumPagesFr(numPages);
-    }
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   }
 
+  function CustomTabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  
   return (
     <Box
       display={"flex"}
@@ -26,18 +45,20 @@ export default function About() {
       alignItems={"center"}
       mt={"3rem"}
     >
-      <Document file="main_en.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPagesEn}
-      </p>
-      <Document file="main_fr.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPagesFr}
-      </p>
+      <Tabs value={value} onChange={handleChange}>
+        <Tab label="English" />
+        <Tab label="French" />
+      </Tabs>
+      <CustomTabPanel value={value} index={0}>
+        <Document file="main_en.pdf" >
+          <Page pageNumber={pageNumber} />
+        </Document>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <Document file="main_fr.pdf" >
+          <Page pageNumber={pageNumber} />
+        </Document>
+      </CustomTabPanel>
     </Box>
   );
 }
