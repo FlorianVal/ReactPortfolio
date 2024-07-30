f3788f7590d4fe0075df911cf4e45f3580b639d8
Florian Valade <florian_val@outlook.fr>
2024-07-30 23:42:58 +0200

    smaller photo

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,31 +5,31 @@ import classNames from 'classnames';
 import EmojiBullet from "./EmojiBullet";
 import SocialIcon from "./SocialIcon";
 import SocialIconSvg from "./SocialIconSvg";
-import {Box} from "@mui/material";
-import {info} from "../../resources/info/Info";
+import { Box } from "@mui/material";
+import { info } from "../../resources/info/Info";
 
 export default function Home() {
 
    return (
-      <Box component={'main'} display={'flex'} flexDirection={{xs: 'column', md: 'row'}} alignItems={'center'}
-           justifyContent={'center'} minHeight={'calc(100vh - 175px)'}>
-         <Box className={classNames(Style.avatar, Style.shadowed)} alt={'image of developer'} style={{background: info.gradient}} component={'img'} src={me} width={{xs: '35vh', md: '39.375vh'}}
-              height={{xs: '32vh', md: '36vh'}}
-              borderRadius={'50%'} p={'0.75rem'} mb={{xs: '1rem', sm: 0}} mr={{xs: 0, md: '2rem'}}/>
+      <Box component={'main'} display={'flex'} flexDirection={{ xs: 'column', md: 'row' }} alignItems={'center'}
+         justifyContent={'center'} minHeight={'calc(100vh - 175px)'}>
+         <Box className={classNames(Style.avatar, Style.shadowed)} alt={'image of developer'} style={{ background: info.gradient }} component={'img'} src={me} width={{ xs: '28vh', md: '31.5vh' }}
+            height={{ xs: '25.6vh', md: '28.8vh' }}
+            borderRadius={'50%'} p={'0.75rem'} mb={{ xs: '1rem', sm: 0 }} mr={{ xs: 0, md: '2rem' }} />
          <Box>
-            <h1>Hi, I'm <span style={{background: info.gradient, WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent'}}>{info.firstName}</span><span className={Style.hand}>🤚</span>
+            <h1>Hi, I'm <span style={{ background: info.gradient, WebkitBackgroundClip: 'text', WebkitTextFillColor: 'transparent' }}>{info.firstName}</span><span className={Style.hand}>🤚</span>
             </h1>
             <h2>I'm {info.position}.</h2>
             <Box component={'ul'} p={'0.8rem'}>
                {info.miniBio.map((bio, index) => (
-                  <EmojiBullet key={index} emoji={bio.emoji} text={bio.text}/>
+                  <EmojiBullet key={index} emoji={bio.emoji} text={bio.text} />
                ))}
             </Box>
             <Box display={'flex'} gap={'1.5rem'} justifyContent={'center'} alignItems={'center'}>
                {info.socials.map((social, index) => (
                   'icon' in social ?
-                  <SocialIcon key={index} link={social.link} icon={social.icon} label={social.label} /> :
-                  <SocialIconSvg key={index} link={social.link} iconPath={social.path} label={social.label} />
+                     <SocialIcon key={index} link={social.link} icon={social.icon} label={social.label} /> :
+                     <SocialIconSvg key={index} link={social.link} iconPath={social.path} label={social.label} />
                ))}
             </Box>
          </Box>
