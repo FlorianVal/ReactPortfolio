859fc22f224460b0a81b812407110c0e7c5c9592
Florian  Valade <florian_val@outlook.fr>
2024-09-18 15:23:09 +0200

    add vercel speed insights

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>,
   document.getElementById('root')
 );
